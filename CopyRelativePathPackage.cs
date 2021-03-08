887ddd2abd2b5be06a5d83e21e5371909588d5df
mere-human <9664141+mere-human@users.noreply.github.com>
2021-03-08 12:59:57 +0200

    Move OptionPageGrid to separate file

--- CopyRelativePathPackage.cs
+++ CopyRelativePathPackage.cs
@@ -2,7 +2,6 @@
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
 using System;
-using System.ComponentModel;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Task = System.Threading.Tasks.Task;
@@ -96,34 +95,5 @@ namespace CopyRelativePath
         }
 
         #endregion
-    }
-
-    public class OptionPageGrid : DialogPage
-    {
-        public const string CategoryName = "Copy Relative Path Extension";
-        public const string PageName = "General";
-
-        [Category(CategoryName)]
-        [DisplayName("Base Path")]
-        [Description("Specify a base for relative path. If empty, then the solution directory is used.")]
-        public string OptionBasePath
-        {
-            get;
-            set;
-        }
-
-        [Category(CategoryName)]
-        [DisplayName("Prefix")]
-        [Description("Specify a prefix to append before the relative path. Example: https://github.com/vim/vim/blob/master/")]
-        public string OptionPrefix
-        {
-            get;
-            set;
-        }
-
-        [Category(CategoryName)]
-        [DisplayName("Use forward slash '/'")]
-        [Description("Replace '\\' by '/' in a path.")]
-        public bool OptionIsForwardSlash { get; set; } = true;
     }
 }
---
+++ OptionPageGrid.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System.ComponentModel;
+
+namespace CopyRelativePath
+{
+    public class OptionPageGrid : DialogPage
+    {
+        public const string CategoryName = "Copy Relative Path Extension";
+        public const string PageName = "General";
+
+        [Category(CategoryName)]
+        [DisplayName("Base Path")]
+        [Description("Specify a base for relative path. If empty, then the solution directory is used.")]
+        public string OptionBasePath
+        {
+            get;
+            set;
+        }
+
+        [Category(CategoryName)]
+        [DisplayName("Prefix")]
+        [Description("Specify a prefix to append before the relative path. Example: https://github.com/vim/vim/blob/master/")]
+        public string OptionPrefix
+        {
+            get;
+            set;
+        }
+
+        [Category(CategoryName)]
+        [DisplayName("Use forward slash '/'")]
+        [Description("Replace '\\' by '/' in a path.")]
+        public bool OptionIsForwardSlash { get; set; } = true;
+    }
+}
